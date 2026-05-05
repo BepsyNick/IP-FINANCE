5928e15c4b89fb728ce577ebf5aa72d9ef66a8be
BepsyNick <melnikovnikita691@gmail.com>
2026-05-06 01:40:42 +0200

    Update fallback route for Express 5 compatibility

--- server.js
+++ server.js
@@ -499,10 +499,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("*", (req, res) => {
+// Express 5 compatible fallback
+app.get(/^(?!\/api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
