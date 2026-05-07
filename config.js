99612d30173d4c18d5f239f08c49dd79dc5f6254
BepsyNick <melnikovnikita691@gmail.com>
2026-05-07 22:40:22 +0200

    Update app version to 1.0.7

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const APP_CONFIG = {
   sessionDurationMs: 60 * 60 * 1000,
   defaultBudget: 600,
   currency: "€",
-  appVersion: "1.0.6",
+  appVersion: "1.0.7",
   monthNames: [
     "Январь",
     "Февраль",
